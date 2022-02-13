72320908e65813ceaa67623dd7fc7e2c4fdabdb2
VilenskayaAlina <99177250+VilenskayaAlina@users.noreply.github.com>
2022-02-14 00:03:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	while (a > 0 && b > 0)
-		if (a > b)
-			a %= b;
-		else
-			b %= a;
+	while ((a != 0) && (b != 0)) {
+		if (a > b) {
+			a = a % b;
+		}
+		else {
+			b = b % a;
+		}
+	}
 	return a + b;
 }
